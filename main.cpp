178ce61b6c0455448ff17f553ff4003de15ab744
Matteo Croce <matteo@openwrt.org>
2015-04-29 14:47:13 +0200

    set app name

--- main.cpp
+++ main.cpp
@@ -5,6 +5,8 @@
 int main(int argc, char *argv[])
 {
 	QApplication a(argc, argv);
+	a.setOrganizationName("TemplateSoft");
+	a.setApplicationName("Template");
 	Template app;
 	app.move((QApplication::desktop()->screenGeometry(&app).width() - app.width()) / 2, (QApplication::desktop()->screenGeometry(&app).height() - app.height()) / 2);
 	app.setVisible(true);
